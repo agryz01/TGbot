e342308df760b18883a3b115b38ffdc617112490
agryz01 <agryz@yandex.ru>
2023-07-27 14:06:19 +0300

    buton

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import config
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}! {config.DESCRIPTION}')
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('начать работу', callback_data='data_collection'))
+    markup.add(types.InlineKeyboardButton('изучить преимущества', callback_data='advantages'))
+    bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}!\n{config.DESCRIPTION}', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'data_collection':
+        bot.send_message(callback.message.chat.id, f'Введите ваш возраст')
+    if callback.data == 'advantages':
+        bot.send_message(callback.message.chat.id, f'{config.ADVANTAGES}')
 
 bot.polling(none_stop=True)
